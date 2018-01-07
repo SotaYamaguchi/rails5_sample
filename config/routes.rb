5e33c3478176ec6eff05a1dfbd37ea05dc1a132f
Sota <area029_0709_sub@yahoo.co.jp>
2018-01-08 02:24:25 +0900

    update view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  # postsフォルダ内ページ表示用
+  get "posts/edit" => "posts#edit"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/show" => "posts#show"
+
+  # postsフォルダ内投稿編集用メソッド
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+
+  # homeフォルダ内ページ表示用
+  get "/" => 'home#top'
+  get "/home/about" => 'home#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
